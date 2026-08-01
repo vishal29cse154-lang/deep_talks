29e2b9d529599296d8f708421626524bbd836618
Vishal Shukla <vishal@example.com>
2026-08-01 18:55:31 +0530

    fix: remove afterEvaluate block causing flutter-gradle-plugin conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,13 +13,3 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
-subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            if (android.namespace == null) {
-                android.namespace = project.group.toString()
-            }
-        }
-    }
-}
